dafe7e696edd8a83b97cda903aae0ca4bfecf67a
s19110 <s19110@pjwstk.edu.pl>
2020-03-26 15:13:41 +0100

    Pierwsza wersja zadań z 3 ćwiczeń APBD

---
+++ Cw3/Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw3.DAL;
+using Cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+      //  [HttpGet]
+      //  public string GetStudents(string orderBy)
+      //  {
+      //      return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+      //  }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            //if (id == 1) {
+            //    return Ok("Kowalski");
+            //} else if (id == 2)
+            //{
+            //    return Ok("Malewski");
+            //}
+            //return NotFound("Nie znaleziono studenta");
+            Student student = _dbService.GetStudents().ToList().Find(i => i.IdStudent == id);
+            if (student == null)
+                return NotFound("Nie znaleziono studenta");
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            _dbService.GetStudents().ToList().Add(student);
+            return Ok(student);
+        }
+
+        //Zadanie 7
+        [HttpPut("{id}")]
+        public IActionResult updateStudent(int id)
+        {
+            Student student = _dbService.GetStudents().ToList().Find(i => i.IdStudent == id);
+            if (student == null)
+                return BadRequest($"Student o id {id} nie został znaleziony");
+            else 
+            {
+                student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+                return Ok($"Student o id {id} zaktualizowany pomyślnie");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult deleteStudent(int id)
+        {
+            Student student = _dbService.GetStudents().ToList().Find(i => i.IdStudent == id);
+            if (student == null)
+                return BadRequest($"Student o id {id} nie został znaleziony");
+            //Linijka poniżej nic nie robi, ale chciałem "zasymulować" usuwanie
+             _dbService.GetStudents().ToList().Remove(student);
+            return Ok($"Student {id} usunięty pomyślnie");
+        }
+    }
+
+}
